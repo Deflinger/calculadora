cdb6535bff5a28f323f8a2b01fb6cdbd90e7e73b
Deflinger <cesaralejandrovirgenherrera@gmail.com>
2019-08-10 12:13:39 -0500

    Tercer Commit de feature

--- init.java
+++ init.java
@@ -4,5 +4,6 @@ public class init
 	System.out.println("Bienvenido a mi juego");
 	System.out.println("Nunca podras salir");
 	System.out.println("Tendras que llegar al final");
+	System.out.println("lograr salir");
 	}
 }
