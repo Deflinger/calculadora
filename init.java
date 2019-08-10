ff8a684888b1abbfe74a25ff216e02fbb31d8d88
Deflinger <cesaralejandrovirgenherrera@gmail.com>
2019-08-10 12:12:00 -0500

    egundo commit de Feature

--- init.java
+++ init.java
@@ -3,5 +3,6 @@ public class init
 	public static void main(String args[]){
 	System.out.println("Bienvenido a mi juego");
 	System.out.println("Nunca podras salir");
+	System.out.println("Tendras que llegar al final");
 	}
 }
